3ae5109fb51a83d2e1ee21ae38b1186438d8c019
Matthew Metzger <mmetzger2385@gmail.com>
2020-11-28 14:51:00 -0500

    added looped guessing

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,34 @@ use std::cmp::Ordering;
 fn main() {
     let low = 1;
     let high = 10;
+    let mut count = 0;
 
     println!("Guess the number!");
 
-    println!("What's yer guess? ({}-{})", low, high);
-
     let secret_number = rand::thread_rng().gen_range(low, high+1);
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+        println!("What's yer guess? ({}-{})", low, high);
 
-    let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        count = count + 1;
 
-    match  guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("Nailed it!"),
+        let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Nailed it!");
+                break;
+            }
+        }
     }
 
-    println!("The secret number was: {}", secret_number);
+    println!("It took you {} guesses!", count);
 }
