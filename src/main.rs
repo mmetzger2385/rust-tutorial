b7af6563417beea7ab804761d09078bbf0462417
Matthew Metzger <mmetzger2385@gmail.com>
2020-11-28 14:42:35 -0500

    added number comparison

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     let low = 1;
@@ -15,7 +16,15 @@ fn main() {
 
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+
     println!("You guessed: {}", guess);
+
+    match  guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("Nailed it!"),
+    }
 
     println!("The secret number was: {}", secret_number);
 }
