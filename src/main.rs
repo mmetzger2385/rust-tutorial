38dc5e2cd48cece8bd17a9c2f0e652c89c129735
Matthew Metzger <mmetzger2385@gmail.com>
2020-11-28 10:43:21 -0500

    added random number generation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
+    let low = 1;
+    let high = 10;
+
     println!("Guess the number!");
 
-    println!("What's yer guess?");
+    println!("What's yer guess? ({}-{})", low, high);
+
+    let secret_number = rand::thread_rng().gen_range(low, high+1);
 
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    println!("The secret number was: {}", secret_number);
 }
