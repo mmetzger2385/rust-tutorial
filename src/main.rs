086192821dd1d0645db11a042e368eb8b66578af
Matthew Metzger <mmetzger2385@gmail.com>
2020-11-29 14:59:49 -0500

    Added constants, tuples, comments

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,31 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    let low = 1;
-    let high = 10;
+
+    //constants
+    const LOW: u32 = 1;
+    const HIGH: u32 = 10;
+
+    //mutable variable
     let mut count = 0;
 
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(low, high+1);
+    //Variables are immutable by default
+    //gen_range - the lower bound is inclusive. Upper bound is exclusive so add 1
+    let secret_number = rand::thread_rng().gen_range(LOW, HIGH+1);
 
     loop {
+
+        //use mut to make variables mutable
         let mut guess = String::new();
 
-        println!("What's yer guess? ({}-{})", low, high);
+        println!("What's yer guess? ({}-{})", LOW, HIGH);
 
         io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+        //shadowing a variable, essentially creates a new variable
+        //can be a different type
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -41,4 +51,16 @@ fn main() {
     }
 
     println!("It took you {} guesses!", count);
+
+    //---tuples--//
+    let guess_count = (2, 5, 8);
+
+    //destructuring
+    let (low, medium, high) = guess_count;
+
+    //direct reference
+    let low_guess = guess_count.0;
+    let medium_guess = guess_count.1;
+    let high_guess = guess_count.2;
+
 }
