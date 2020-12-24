cb4c32b16e2e7a2212c90ebf94c7ac962d577611
Matthew Metzger <mmetzger2385@gmail.com>
2020-12-24 16:40:37 -0500

    functions!

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,46 @@ fn main() {
     const LOW: u32 = 1;
     const HIGH: u32 = 10;
 
-    //mutable variable
-    let mut count = 0;
-
     println!("Guess the number!");
 
-    //Variables are immutable by default
+    game_loop(LOW,HIGH);
+
+    //---arrays---//
+    //arrays cannot change size
+    let guess_count_arr: [i32; 3] = [2, 5, 8];
+
+    //---tuples--//
+    //just copying an array into a tuple, for no good reason.
+    let guess_count = (guess_count_arr[0], guess_count_arr[1], guess_count_arr[2]);
+
+    //destructuring
+    let (low, medium, high) = guess_count;
+
+    //direct reference
+    let low_guess = guess_count.0;
+    let medium_guess = guess_count.1;
+    let high_guess = guess_count.2;
+}
+
+fn generate_secret_number(low:u32, high:u32) -> u32{
     //gen_range - the lower bound is inclusive. Upper bound is exclusive so add 1
-    let secret_number = rand::thread_rng().gen_range(LOW, HIGH+1);
+    rand::thread_rng().gen_range(low, high+1)
+}
+
+fn game_loop(low: u32, high: u32){
+
+    //Variables are immutable by default
+    let secret_number = generate_secret_number(low,high);
+
+    //mutable variable
+    let mut count = 0;
 
     loop {
 
         //use mut to make variables mutable
         let mut guess = String::new();
 
-        println!("What's yer guess? ({}-{})", LOW, HIGH);
+        println!("What's yer guess? ({}-{})", low, high);
 
         io::stdin().read_line(&mut guess).expect("Failed to read line");
 
@@ -51,16 +76,5 @@ fn main() {
     }
 
     println!("It took you {} guesses!", count);
-
-    //---tuples--//
-    let guess_count = (2, 5, 8);
-
-    //destructuring
-    let (low, medium, high) = guess_count;
-
-    //direct reference
-    let low_guess = guess_count.0;
-    let medium_guess = guess_count.1;
-    let high_guess = guess_count.2;
 
 }
